0446e72d5f98d7c887d87f0c3ded6a15f9b0e826
Israel Campillay <israelcampy@gmail.com>
2025-02-04 18:40:05 -0300

    config Vercel

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const path = require('path');
 
 const app = express();
 
 // Habilita CORS
 app.use(cors());
+
+// Sirve los archivos estáticos de React
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Ruta para obtener las órdenes
 app.get('/api/ordenes', async (req, res) => {
@@ -26,5 +30,17 @@ app.post('/api/ordenes', async (req, res) => {
     res.status(500).json({ error: 'Error al crear la orden' });
   }
 });
+
+// Ruta para servir index.html en la raíz
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Inicia el servidor
+const PORT = 3001;
+//app.listen(PORT, () => {
+ // console.log(`Servidor proxy corriendo en http://localhost:${PORT}`);
+//}
+
 
 module.exports = app;
